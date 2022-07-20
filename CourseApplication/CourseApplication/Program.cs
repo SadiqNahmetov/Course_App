e492ca470b12f6cf7e3fdab3d44d2221579a6e0d
SadiqNahmetov <sadigfn@code.edu.az>
2022-07-20 21:26:25 +0400

    Console App

---
+++ CourseApplication/CourseApplication/Program.cs
@@ -0,0 +1,61 @@
+﻿using Service.Helpers;
+using System;
+
+namespace CourseApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper.WriteConsole(ConsoleColor.Blue, "Select one option: ");
+            Helper.WriteConsole(ConsoleColor.Yellow, "1 - Create Group, 2 - Update group, 3 - Delete Group, 4 - Get group by id");
+
+            while (true)
+            {
+            SelectOption : string selectOption = Console.ReadLine();
+                int selectTrueOption;
+                bool isSelectOption = int.TryParse(selectOption, out selectTrueOption);
+
+                if (isSelectOption)
+                {
+                    switch (selectTrueOption)
+                    {
+                        case 1:
+                            Helper.WriteConsole(ConsoleColor.Blue, "Add group name: ");
+                            string groupName = Console.ReadLine();
+
+                            Helper.WriteConsole(ConsoleColor.Blue, "Add teacher name: ");
+
+                            string groupTeacher = Console.ReadLine();
+                            string teacherCount;
+                            
+                            break;
+
+                        case 2:
+                            Console.WriteLine(selectTrueOption);
+                            break;
+
+
+                        case 3:
+                            Console.WriteLine(selectTrueOption);
+                            break;
+
+
+                        case 4:
+                            Console.WriteLine(selectTrueOption);
+                            break;
+
+                        default:
+                            Helper.WriteConsole(ConsoleColor.Red, "Select correct option number: ");
+                            break;
+                    }
+                }
+                else
+                {
+                    Helper.WriteConsole(ConsoleColor.Red, "Select correct option: ");
+                    goto SelectOption;
+                }
+            }
+        }
+    }
+}
---
+++ CourseApplication/Service/Services/GroupService.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Models;
+using Repository.Repositories;
+using Service.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Service.Services
+{
+    public class GroupService : IGroupService
+    {
+        private GroupRepository _groupRepository;
+        private int _count;
+        public GroupService()
+        {
+            _groupRepository = new GroupRepository();
+        }
+        public Group Create(Group group)
+        {
+            group.Id = _count;
+            _groupRepository.Create(group);
+            _count++;
+            return group;
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Group> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Update(int id, Group group)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
